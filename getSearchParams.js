b5c197037e93313e18469620abc421de018a0834
Gabriel Simek <gabrielsimek@outlook.com>
2021-07-15 22:22:34 -0700

    for heroku

--- getSearchParams.js
+++ getSearchParams.js
@@ -1,10 +1,9 @@
-const getSearchAndCity = (message) => {
+const getSearchParams = (message) => {
     const searchTerm = message.split(' ').slice(3, 4).join('')
     const city = message.split(' ').slice(5, 6 ).join('')
     return [searchTerm, city]
 }
-//I want a couch in portland
-// const search = getSearchAndCity('I want a couch in portland')
+
 module.exports = {
-    getSearchAndCity
+    getSearchParams
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const fetch = require('node-fetch')
 const { urlencoded } = require('body-parser');
 const { createResponse } = require('./createResponse.js')
-const { getSearchAndCity } = require('./getSearchAndCity.js')
+const { getSearchParams } = require('./getSearchParams.js')
 
 
 const app = express();
@@ -14,13 +14,14 @@ app.use(urlencoded({ extended: false }));
 const URL = 'https://project-scrape.herokuapp.com/api/v1/results';
 
 app.post('/sms', async (req, res) => {
-  console.log(req.body.Body);
+  
   const params = getSearchAndCity(req.body.Body)
- const twiml = new MessagingResponse();
-  console.log(params)
- const response =  await fetch(`${URL}/${params[0]}/${params[1]}`, {
-  method: 'GET'
+  const twiml = new MessagingResponse();
+ 
+  const response =  await fetch(`${URL}/${params[0]}/${params[1]}`, {
+     method: 'GET'
 })
+
 const items = await response.json()
 const slicedItems = items.slice(0, 3)
 const message = createResponse(slicedItems, 'chair', 'portland')
